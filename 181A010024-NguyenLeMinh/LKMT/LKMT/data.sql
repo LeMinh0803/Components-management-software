7609519bffb45aaffaf92c5ad69bcc9e0ac07992
LeMinh0803 <80593408+LeMinh0803@users.noreply.github.com>
2023-06-03 15:52:28 +0700

    Initial commit

---
+++ 181A010024-NguyenLeMinh/LKMT/LKMT/data.sql
@@ -0,0 +1,11 @@
+﻿insert into admin (email,matkhau,ten,sodienthoai) values ('minh','123','Nguyen Le Minh','0766258213')
+insert into admin (email,matkhau,ten,sodienthoai) values ('admin','admin','nguyen le admin','0766258213')
+
+
+insert into nhomsanpham values ('RAM','Ram - Bộ nhớ trong')
+insert into nhomsanpham values ('CPU','Cpu - Bộ vi xử lí')
+insert into nhomsanpham values ('MAIN','Mainboard - Bo mạch chủ')
+insert into nhomsanpham values ('SSD','SSD - Ổ cứng thể rắn')
+insert into nhomsanpham values ('HDD','HDD - Ổ cứng PC')
+
+insert into loaisanpham values ('HDD','HDD - Ổ cứng PC')
